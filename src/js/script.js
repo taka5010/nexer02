2e39daa5a71a87782e608b44f3048a3e45ad69f7
taka <blue_snow_2245@yahoo.co.jp>
2025-09-14 04:10:52 +0900

    202509150410

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,44 @@
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+jQuery(function ($) {
+  const $hamburger = $(".js-hamburger");
+  const $drawer = $(".js-drawer");
 
+  $hamburger.click(function () {
+      if ($(this).hasClass("is-open")) {
+          $(this).find("span:nth-of-type(1), span:nth-of-type(2), span:nth-of-type(3)").addClass("is-close");
+          setTimeout(() => {
+              $(this).removeClass("is-open");
+              $(this).find("span:nth-of-type(1), span:nth-of-type(2), span:nth-of-type(3)").removeClass("is-close");
+          }, 500);
+      } else {
+          $(this).addClass("is-open");
+      }
+      $drawer.fadeToggle();
+  });
+
+  $(".js-drawer a[href]").on("click", function () {
+      $hamburger.removeClass("is-open");
+      $drawer.fadeOut();
+  });
 
 });
+
+
+// ドロワーのメニューをクリックしたら開く
+document.addEventListener("DOMContentLoaded", function () {
+    const toggles = document.querySelectorAll(".header-drawer__toggle");
+  
+    toggles.forEach(toggle => {
+      toggle.addEventListener("click", function () {
+        const submenu = this.nextElementSibling;
+        
+        // トグルボタン自体にもopenクラスを追加/削除
+        this.classList.toggle("open");
+        
+        // サブメニューの開閉
+        if (submenu) {
+          submenu.classList.toggle("open");
+        }
+      });
+    });
+  });
