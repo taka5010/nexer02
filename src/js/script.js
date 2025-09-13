39a996111897df0534ca9ed703633d66ccbe9f93
taka <blue_snow_2245@yahoo.co.jp>
2025-09-14 06:48:59 +0900

    202509140645

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,34 @@
 
 jQuery(function ($) {
-  const $hamburger = $(".js-hamburger");
-  const $drawer = $(".js-drawer");
+    // ハンバーガー
+    const $hamburger = $(".js-hamburger");
+    const $drawer = $(".js-drawer");
+    $hamburger.click(function () {
+        if ($(this).hasClass("is-open")) {
+                $(this).find("span:nth-of-type(1), span:nth-of-type(2), span:nth-of-type(3)").addClass("is-close");
+            setTimeout(() => {
+                $(this).removeClass("is-open");
+                $(this).find("span:nth-of-type(1), span:nth-of-type(2), span:nth-of-type(3)").removeClass("is-close");
+            }, 500);
+        } else {
+            $(this).addClass("is-open");
+        }
+        $drawer.fadeToggle();
+    });
+    $(".js-drawer a[href]").on("click", function () {
+        $hamburger.removeClass("is-open");
+        $drawer.fadeOut();
+    });
 
-  $hamburger.click(function () {
-      if ($(this).hasClass("is-open")) {
-          $(this).find("span:nth-of-type(1), span:nth-of-type(2), span:nth-of-type(3)").addClass("is-close");
-          setTimeout(() => {
-              $(this).removeClass("is-open");
-              $(this).find("span:nth-of-type(1), span:nth-of-type(2), span:nth-of-type(3)").removeClass("is-close");
-          }, 500);
-      } else {
-          $(this).addClass("is-open");
-      }
-      $drawer.fadeToggle();
-  });
-
-  $(".js-drawer a[href]").on("click", function () {
-      $hamburger.removeClass("is-open");
-      $drawer.fadeOut();
-  });
-
+    // ドロワーのメニュー内のアコーディオンメニュー
+    $(".header-drawer__toggle").on("click", function() {
+        const $this = $(this);
+        const $submenu = $this.next(".header-drawer__submenu");
+        $this.toggleClass("open");
+        if ($submenu.length) {
+            $submenu.toggleClass("open");
+        }
+    });
 });
 
 
-// ドロワーのメニューをクリックしたら開く
-document.addEventListener("DOMContentLoaded", function () {
-    const toggles = document.querySelectorAll(".header-drawer__toggle");
-  
-    toggles.forEach(toggle => {
-      toggle.addEventListener("click", function () {
-        const submenu = this.nextElementSibling;
-        
-        // トグルボタン自体にもopenクラスを追加/削除
-        this.classList.toggle("open");
-        
-        // サブメニューの開閉
-        if (submenu) {
-          submenu.classList.toggle("open");
-        }
-      });
-    });
-  });
